15ec99f6bb426199123fe31fdc127401eb290ef3
connormunro <cmunro1@binghamton.edu>
2019-11-06 11:42:58 -0500

    11/06

--- Kindergarten Snap Words/Kindergarten Snap Words/ViewController.swift
+++ Kindergarten Snap Words/Kindergarten Snap Words/ViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    
+    init()
+    {
+       
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func exitAbout(_ sender: UIButton)
+    {
+        //aboutView.dismiss(animated: false, completion: nil)
+    }
+    
+    @IBAction func openAbout(_ sender: UIButton)
+    {
+        
+    }
+    
 }
 
