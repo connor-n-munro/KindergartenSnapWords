c4c20d61a65084558b4e5424c16ee3acb28c9808
connormunro <cmunro1@binghamton.edu>
2019-11-08 11:27:22 -0500

    11/08
    Finished work on main screen

--- Kindergarten Snap Words/Kindergarten Snap Words/ViewController.swift
+++ Kindergarten Snap Words/Kindergarten Snap Words/ViewController.swift
@@ -2,33 +2,36 @@
 //  ViewController.swift
 //  Kindergarten Snap Words
 //
-//  Created by Connor Munro on 11/4/19.
-//  Copyright © 2019 Connor Munro. All rights reserved.
+//  Created by Connor Munro & Ron Gunczler on 11/4/19.
+//  Copyright © 2019 Connor Munro & Ron Gunczler. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController
 {
-    
-    init()
-    {
-       
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.viewWithTag(3)?.isHidden = true;
     }
     @IBAction func exitAbout(_ sender: UIButton)
     {
-        //aboutView.dismiss(animated: false, completion: nil)
-    }
-    
-    @IBAction func openAbout(_ sender: UIButton)
-    {
+        switch sender.tag
+        {
+        case 0: //hit the about button, show the about page
+            self.view.viewWithTag(3)?.isHidden = false;
+            break
+        case 1: //hit the return button on the about page, return to main screen
+            self.view.viewWithTag(3)?.isHidden = true;
+            break
+        default:
+            print("broken")
+        }
         
     }
+    
     
 }
 
