019a915ff3d43dc8c384bffefb5bfc04beded10d
connormunro <cmunro1@binghamton.edu>
2019-11-20 14:42:35 -0500

    Directory Fix

---
+++ Kindergarten Snap Words/Kindergarten Snap Words/AboutTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AboutTableViewCell.swift
+//  Kindergarten Snap Words
+//
+//  Created by Kristen Munro on 11/20/19.
+//  Copyright © 2019 Connor Munro. All rights reserved.
+//
+
+import UIKit
+
+class AboutTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
