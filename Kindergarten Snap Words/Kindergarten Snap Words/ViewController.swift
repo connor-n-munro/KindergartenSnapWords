164b8c60a0a5bb7785f01a2c9819c2b7dfe2dd89
RGunczl1 <rgunczl1@binghamton.edu>
2019-12-06 16:57:21 -0500

    quick fix for UI
    UI is not working optimally, but gets to all the places it needs to be

--- Kindergarten Snap Words/Kindergarten Snap Words/ViewController.swift
+++ Kindergarten Snap Words/Kindergarten Snap Words/ViewController.swift
@@ -26,6 +26,9 @@ class ViewController: UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
     }
    /* @IBAction func switchAbout(_ sender: UIButton)
     {
